fb1cec84786f4f6f6801961bffa0f663b9d53671
Saule Tussupbekova <sauletuss@gmail.com>
2021-06-17 17:42:02 +0600

    ДЗ: Тут должно пройти

--- tests/functional/FindPrintedDressCest.php
+++ tests/functional/FindPrintedDressCest.php
@@ -17,6 +17,6 @@ class FindPrintedDressCest
         $I->seeElement('#search_query_top');
         $I->fillField('#search_query_top', 'Printed dress');
         $I->click('#searchbox > button');
-        $I->seeNumberOfElements('.product-container', 4);
+        $I->seeNumberOfElements('.product-container', 5);
     }
 }
